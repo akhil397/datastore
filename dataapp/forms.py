a115cee67b4f439351aee9b92458d0c80f80d365
akhil <akhil@gmail.com>
2024-06-26 13:39:59 +0530

    Form update error solved

--- dataapp/forms.py
+++ dataapp/forms.py
@@ -6,10 +6,10 @@ class CodeForm(forms.ModelForm):
         model = CodeModels
         fields = ['title', 'code_lang', 'codes', 'description', 'type_code', 'image']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title'}),
-            'code_lang': forms.Select(attrs={'class': 'form-control'}),  # Assuming this is a foreign key dropdown
-            'codes': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter code'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter description'}),
-            'type_code': forms.Select(attrs={'class': 'form-control'}),  # Assuming this is a foreign key dropdown
-            'image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'code_lang': forms.Select(attrs={'class': 'form-control'}),
+            'codes': forms.Textarea(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'type_code': forms.Select(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
--- dataapp/views.py
+++ dataapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.db.models import Q
 from .models import CodeModels, Language, Types
 from .forms import CodeForm
 
-
-def details(request):
+def details(request):#this for listing
     code_models = CodeModels.objects.all()
     return render(request, 'detail.html', {'code_models': code_models})
 
-def about(request, pk):
+def about(request, pk):#this for detailing
     code_detail = get_object_or_404(CodeModels, id=pk)
     return render(request, 'about.html', {'code_detail': code_detail})
 
@@ -27,31 +28,58 @@ def adddata(request):
             return redirect('details')  # Redirect to the 'details' view after saving
 
     return render(request, 'add_data.html', context)
+
 def update_code_model(request, pk):
     code_model = get_object_or_404(CodeModels, id=pk)
+    all_lang = Language.objects.all()
+    types_code = Types.objects.all()
     
     if request.method == 'POST':
         form = CodeForm(request.POST, request.FILES, instance=code_model)
+        context = {
+            'all_langs': all_lang,
+            'type_codes': types_code,
+            'form': form
+        }
         if form.is_valid():
             form.save()
-            return redirect('about', pk=code_model.pk)  # Redirect to a detail view or any other view
+            messages.success(request, 'Code model updated successfully!')
+            return redirect('about', pk=code_model.pk)
+        else:
+            messages.error(request, 'Please correct the error(s) below.')
     else:
         form = CodeForm(instance=code_model)
-    
-    return render(request, 'update.html', {'form': form})
-
+        context = {
+            'all_langs': all_lang,
+            'type_codes': types_code,
+            'form': form
+        }
+    return render(request, 'update.html', context)
 def delete_data(request, pk):
     code = get_object_or_404(CodeModels, id=pk)
     code.delete()
     return redirect('details')
 
+
 def search_btn(request):
     if request.method == 'POST':
-        searched= request.POST['searched']
-        # Filter your model by the search query
-        posts = CodeModels.objects.filter(language__contains=searched)
-        return render(request, 'search.html',{'searched':searched,'posts':posts})
-    else :
+        searched = request.POST['searched']
+        if searched:
+            posts = CodeModels.objects.filter(
+                Q(title__icontains=searched) |
+                Q(code_lang__name__icontains=searched) |  # Assuming code_lang is a ForeignKey
+                Q(codes__icontains=searched) |
+                Q(type_code__name__icontains=searched)  # Assuming type_code is a ForeignKey
+            )
+            if posts.exists():
+                messages.success(request, f'Results for "{searched}" found.')
+            else:
+                messages.error(request, f'No results found for "{searched}".')
+            return render(request, 'search.html', {'searched': searched, 'posts': posts})
+        else:
+            messages.error(request, 'Please enter a search term.')
+            return render(request, 'search.html')
+    else:
         return render(request, 'search.html')
 
         
